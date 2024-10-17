e7ed9a20387aaa152c5897a007c1919a3c2c592d
ethanc8 <echaroenpitaks@gmail.com>
2024-10-17 14:10:13 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# titan-forge
+
+## Acknowledgements
+
+This forge's build scripts are based on Wolf Vollprecht's https://github.com/wolfv/rust-forge.
---
+++ opencv-titan/opencl-build.sh
@@ -0,0 +1,36 @@
+mkdir build
+cd build
+
+extra_cmake_args=(
+    -D CMAKE_BUILD_TYPE=RELEASE
+    -D OPENCV_EXTRA_MODULES_PATH="$(realpath ../opencv_contrib/modules)"
+    -D WITH_OPENCL=ON
+    -D ENABLE_FAST_MATH=ON
+    -D WITH_QT=ON
+    -D WITH_OPENMP=ON
+    -D BUILD_TIFF=ON
+    -D WITH_FFMPEG=ON
+    -D WITH_GSTREAMER=ON
+    -D WITH_TBB=ON
+    -D BUILD_TBB=OFF
+    -D BUILD_TESTS=OFF
+    -D WITH_EIGEN=ON
+    -D WITH_PROTOBUF=ON
+    -D OPENCV_ENABLE_NONFREE=ON
+    -D INSTALL_C_EXAMPLES=OFF
+    -D INSTALL_PYTHON_EXAMPLES=OFF
+    -D OPENCV_GENERATE_PKGCONFIG=ON
+    -D BUILD_EXAMPLES=OFF
+)
+
+cmake "${CMAKE_ARGS}" "${extra_cmake_args[@]}" \
+    -DCMAKE_PREFIX_PATH="$PREFIX" \
+    -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+    "$SRC_DIR/opencv"
+
+# -D WITH_V4L=ON
+# -D WITH_LIBV4L=ON
+
+ninja
+
+
