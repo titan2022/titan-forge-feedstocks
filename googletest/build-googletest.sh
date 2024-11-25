66ecc9f6f6b93c40ed2e23df6f49a673854f834f
ethanc8 <echaroenpitaks@gmail.com>
2024-11-25 16:59:10 -0600

    Add googletest

---
+++ googletest/build-googletest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir build
+cd build
+
+extra_cmake_args=(
+    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_INSTALL_LIBDIR=lib
+    -DCMAKE_VERBOSE_MAKEFILE=ON                                          
+)
+
+cmake -GNinja ${CMAKE_ARGS} "${extra_cmake_args[@]}" \
+    -DCMAKE_PREFIX_PATH="$PREFIX" \
+    -DCMAKE_INSTALL_PREFIX="$PREFIX" \
+    "$SRC_DIR"
+
+ninja
+ninja install
